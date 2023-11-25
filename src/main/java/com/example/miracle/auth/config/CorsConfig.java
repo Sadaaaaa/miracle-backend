c0823d469685d200e4a0d69c840df642087e14a9
sadopwnz <sadopwnz@yandex.ru>
2023-11-25 13:34:11 +0200

    cors

--- src/main/java/com/example/miracle/auth/config/CorsConfig.java
+++ src/main/java/com/example/miracle/auth/config/CorsConfig.java
@@ -1,24 +1,24 @@
-//package com.example.miracle.auth.config;
-//
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.CorsRegistry;
-//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//@Configuration
-//@EnableWebMvc
-//public class CorsConfig implements WebMvcConfigurer {
-//
-//    @Value("${api.url}")
-//    private String apiUrl;
-//
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/")
-//                .allowedOrigins("http://localhost:3000", "http://localhost:8090", "http://sadopwnz.duckdns.com", "http://192.168.88.26")
-//                .allowedMethods("GET", "POST", "OPTIONS", "DELETE", "PUT")
-//                .allowedHeaders("*")
-//                .allowCredentials(true);
-//    }
-//}
+package com.example.miracle.auth.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Value("${api.url}")
+    private String apiUrl;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/")
+                .allowedOrigins("http://localhost:3000", "http://localhost:8090", "http://sadopwnz.duckdns.com", "http://192.168.88.26")
+                .allowedMethods("GET", "POST", "OPTIONS", "DELETE", "PUT")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
